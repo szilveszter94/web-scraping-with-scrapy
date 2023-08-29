db4cf605c8dba6433e2c7150255036d0d92b9943
Szilveszter <test@gmail.com>
2023-08-29 20:51:05 +0200

    add page calculator

--- scrape_emails/scrape_emails/spiders/firma_email_spider.py
+++ scrape_emails/scrape_emails/spiders/firma_email_spider.py
@@ -2,32 +2,39 @@ import scrapy
 import openpyxl as openpyxl
 from scrapy import signals
 from scrapy.signalmanager import dispatcher
+import math
 
 
 class QuotesSpider(scrapy.Spider):
     name = "firms"
-    start_urls = []
     accumulated_items = []
+    all_page_urls = []
+    items_per_page = 25
     # you have to provide the filename
     EXCEL_FILE_NAME = "output.xlsx"
     # you have to provide the page url
-    BASE_LINK = "https://www.zoznam.sk/katalog/Spravodajstvo-informacie/Abecedny-zoznam-firiem/A/sekcia.fcgi?sid=1173&so=&page={}&desc=&shops=&kraj=&okres=&cast=&attr="
-    for i in range(1, 2):  # you have to provide the page numbers + 1
-        start_urls.append(BASE_LINK.format(i))
+    BASE_LINK = "https://www.zoznam.sk/katalog/Spravodajstvo-informacie/Abecedny-zoznam-firiem/0-9/sekcia.fcgi?sid=1172&so=&page={}&desc=&shops=&kraj=&okres=&cast=&attr="
+    start_urls = [BASE_LINK.format(1)]
 
     def __init__(self, *args, **kwargs):
         super(QuotesSpider, self).__init__(*args, **kwargs)
         # Connect the spider_closed signal to the handler
         dispatcher.connect(self.handle_spider_closed, signal=signals.spider_closed)
 
-
     def parse(self, response):
+        firma = response.css("small::text")[0].extract()
+        formatted_page_number = int(firma.strip("()"))
+        last_page = math.ceil(formatted_page_number / self.items_per_page)
+        for i in range(1, last_page + 1):
+            yield scrapy.Request(url=self.BASE_LINK.format(i), callback=self.parse_firma)
+
+    def parse_firma(self, response):
         BASE_FIRMA_LINK = "https://www.zoznam.sk"  # base link for profiles (do not modify)
         firma = response.css("a.link_title::attr(href)").extract()
         for link in firma:
-            yield scrapy.Request(url=f'{BASE_FIRMA_LINK}{link}', callback=self.parse_firma)
+            yield scrapy.Request(url=f'{BASE_FIRMA_LINK}{link}', callback=self.parse_email)
 
-    def parse_firma(self, response):
+    def parse_email(self, response):
         emails = response.css('.profile .row .col-sm-9 a::text').extract()
         for email in emails:
             if "@" in email:
